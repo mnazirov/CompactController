7ce2cc4faf2e27a6cd4f1313fb125d5acbdcdf14
Marat Nazirov <nazimovma@sovcombank.ru>
2024-11-13 10:16:47 +0300

    Initialisation

---
+++ CompactController/ViewController.swift
@@ -0,0 +1,15 @@
+//  CompactController
+//
+//  Created by Marat Nazirov on 13.11.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
